88d558fd0ffb6962e1b4e786ab29796818aaa17f
ksieuk <iebebd3@gmail.com>
2024-01-14 19:58:40 +0300

    fix: limits

--- src/lib/methods/first_lab/models.py
+++ src/lib/methods/first_lab/models.py
@@ -4,11 +4,11 @@ import pydantic
 class FirstLabModel(pydantic.BaseModel):
     k: float = pydantic.Field(default=1, description="Коэффициент усиления k")
     t: float = pydantic.Field(default=1, description="Постоянная времени t")
-    xi: float = pydantic.Field(default=0.5, description="xi")
+    xi: float = pydantic.Field(default=0.5, description="Коэффициент демпфирования xi")
 
     @pydantic.field_validator("xi")
     @classmethod
-    def check_limits(cls, value: float) -> float:
+    def check_limits_xi(cls, value: float) -> float:
         assert 0 <= value <= 1, ValueError("Значение не может быть меньше 0")
         return value
 
